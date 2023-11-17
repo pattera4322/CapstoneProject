b1632e096a822381303313ecaf40ac019ba71007
maxuamthon <68773089+maxuamthon@users.noreply.github.com>
2023-11-17 22:12:01 +0700

    new commit from max - 22.11

--- client/src/App.js
+++ client/src/App.js
@@ -14,40 +14,59 @@ function App() {
 
   return (
     <div class="text-center font-amiko">
-      <nav class="fixed top-0 z-40 w-full bg-white border-gray-200 bg-white drop-shadow-lg h-14">
-        <div class="flex flex-wrap items-center justify-between px-8 pt-1" >
-          <NavLink to="/" class="flex items-center" aria-current="page">
-            <img
-              src="assets/SmartStockLogo.svg"
-              class="h-12 mr-4"
-              alt="Smart Stock"
-            />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Smart Stock
-            </span>
-          </NavLink>
-          <div class="md:w-auto">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
-              <li>
-                <NavLink
-                  to="/Login"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Login
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/Register"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Register
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+  <nav class="fixed top-0 z-40 w-full bg-white border-gray-200 bg-white drop-shadow-lg">
+    <div class="flex flex-wrap items-center justify-between px-4 sm:px-8 py-1">
+      <NavLink to="/" class="flex items-center" aria-current="page">
+        <img
+          src="assets/SmartStockLogo.svg"
+          class="h-12 mr-2 sm:mr-4"
+          alt="Smart Stock"
+        />
+        <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
+          Smart Stock
+        </span>
+      </NavLink>
+      <div class="md:hidden">
+       
+        <button id="menuToggle" class="text-gray-600 focus:outline-none">
+          <svg
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <div class="hidden md:flex md:w-auto">
+        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white">
+          <li>
+            <NavLink
+              to="/Login"
+              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/Register"
+              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              Register
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
       <Routes>
         <Route path="/" element={<HomePage />} />
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,35 +10,38 @@ function HomePage() {
   };
 
   return (
-    <div class="pt-24">
-      {/*<------------------------------ Section 1 Introduce web application ------------------------------> */}
-      <div className="mt-10">
-        <h1 className="text-4xl font-bold">
-          Step into a Smarter Retail Experience!
-        </h1>
-        <div className="text-xl mt-10">
-          <p>Maximize the potential of your workforce by automating</p>
-          <p>complexing tasks and optimizing workflows with our</p>
-          <p>predictive stocking data.</p>
-        </div>
-        {/* <div className="pt-60 pb-60 bg-no-repeat bg-bottom bg-auto" style={{ backgroundImage: 'url("/assets/Openedbox.svg")' }}> */}
-        <div className="pt-10 pb-10 bg-no-repeat bg-bottom bg-auto">
-          <button
-            onClick={scrolldown}
-            class="text-xl hover:bg-orange-100 text-black border-2 border-black rounded-full px-10 py-4"
-          >
-            Let’s get start!
-          </button>
-        </div>
-      </div>
-      <img src="/assets/Openedbox.svg" alt="open box" width={3500} />
-
-      {/*<------------------------------ Section 2 Stepper------------------------------> */}
-
-      <div class="importSection pr-20 pl-20" ref={StepSection}>
-        <StepperSection />
-      </div>
+    <div className="pt-24">
+  {/*<------------------------------ Section 1 Introduce web application ------------------------------> */}
+  <div className="mt-10 text-center px-4 sm:px-8">
+    <h1 className="text-4xl sm:text-5xl font-bold">
+      Step into a Smarter Retail Experience!
+    </h1>
+    <div className="text-lg sm:text-xl mt-6">
+      <p>Maximize the potential of your workforce by automating</p>
+      <p>complex tasks and optimizing workflows with our</p>
+      <p>predictive stocking data.</p>
     </div>
+    <div className="pt-8 sm:pt-10 pb-10 bg-no-repeat bg-bottom bg-auto">
+      <button
+        onClick={scrolldown}
+        className="text-lg sm:text-xl hover:bg-orange-100 text-black border-2 border-black rounded-full px-6 sm:px-10 py-3 sm:py-4"
+      >
+        Let’s get started!
+      </button>
+    </div>
+  </div>
+  <img
+    src="/assets/Openedbox.svg"
+    alt="open box"
+    className="mx-auto w-full max-w-2xl sm:max-w-3xl md:max-w-4xl lg:max-w-5xl xl:max-w-6xl"
+  />
+
+  {/*<------------------------------ Section 2 Stepper------------------------------> */}
+
+  <div className="importSection pr-4 pl-4 sm:pr-20 sm:pl-20" ref={StepSection}>
+    <StepperSection />
+  </div>
+</div>
   );
 }
 
