06c16477bf291f3de7f891975d57662260a1cfdb
Kittiphum Uamthon <kittiphum601@gmail.com>
2024-02-07 01:24:32 +0700

    update button

--- client/src/components/DropdownNavbar.js
+++ client/src/components/DropdownNavbar.js
@@ -55,11 +55,6 @@ const Dropdown = ({ isDivider = false, handleSignout, userName }) => {
       {isDropdownVisible && (
         <div className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 absolute mt-2">
           <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
-            <NavLink to="/Dashboard">
-              <li className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                Dashboard
-              </li>
-            </NavLink>
             <NavLink to="/History">
               <li className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                 History
--- client/src/components/History/JobComponent.js
+++ client/src/components/History/JobComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ProgressBar from "../ProgressBar";
 import { useNavigate } from "react-router-dom";
+import ButtonComponent from "../Button";
 
 const JobComponent = ({ job, progressData }) => {
   const navigate = useNavigate();
@@ -13,15 +14,14 @@ const JobComponent = ({ job, progressData }) => {
 
   const OnCompletedJobClick = () => {
     console.log(JSON.stringify(job.data))
-    if(isJobCompleted){
-      navigate('/Dashboard', {replace:true, state : job.data.fileid});
+    if (isJobCompleted) {
+      navigate('/Dashboard', { replace: true, state: job.data.fileid });
     }
   };
 
   return (
     <div
       className={`transition ease-in-out hover:-translate-y-1 hover:scale-10 mx-16 my-8 px-8 py-8 rounded-md text-left shadow-[0px_10px_1px_rgba(221,221,221,1),0_10px_20px_rgba(204,204,204,1)]`}
-      onClick={OnCompletedJobClick}
     >
       <div>Job ID: {job.id}</div>
       {/* Uncomment the line below if job.name is available */}
@@ -37,6 +37,11 @@ const JobComponent = ({ job, progressData }) => {
           />
         )}
         {isJobWaiting && <div>Waiting for analyze...</div>}
+        <div className="mt-4 right-0">
+          <ButtonComponent
+            onClick={OnCompletedJobClick}
+            children={<div>Go to Dashboard</div>} />
+        </div>
       </div>
     </div>
   );
